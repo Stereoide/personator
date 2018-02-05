521f91e4479d40d04cb2ad98160a77d7ab412e94
Jörn Maske <j.maske@otterbach.de>
2018-02-05 08:58:26 +0100

    Updated heroku database config

--- config/database.php
+++ config/database.php
@@ -67,11 +67,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $url['host']),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', substr($url['path'], 1)),
+            'username' => env('DB_USERNAME', $url['user']),
+            'password' => env('DB_PASSWORD', $url['pass']),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
